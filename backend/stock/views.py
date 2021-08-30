1eadb63316bb05b4b7713997ab0ade5255381032
ketkat001 <ketkat001@gmail.com>
2021-08-30 13:42:03 +0900

    backend|stock|create forecast route

--- backend/stock/views.py
+++ backend/stock/views.py
@@ -3,3 +3,7 @@ from django.shortcuts import render
 # Create your views here.
 def main_page(request):
     return render(request, 'stock/main_page.html')
+
+
+def forecasts(request):
+    return render(request, 'stock/forecasts.html')
