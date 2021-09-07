8165e033874aa3e4182621a4a29918d3be9cb7ee
ketkat001 <ketkat001@gmail.com>
2021-09-07 20:41:03 +0900

    backend|stock|create interest function

--- backend/stock/urls.py
+++ backend/stock/urls.py
@@ -6,4 +6,6 @@ app_name = 'stock'
 urlpatterns = [
     path('', views.main_page, name="main_page"),
     path('forecasts/', views.forecasts, name="forecasts"),
+    path('forecasts/<int:stock_pk>/', views.forecast_detail, name="forecast_detail"),
+    path('<int:stock_pk>/interest/', views.interest, name='interest'),
 ]
--- backend/stock/views.py
+++ backend/stock/views.py
@@ -1,4 +1,8 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
+from django.http import HttpResponse, JsonResponse
+from .models import Stock
+
+
 
 # Create your views here.
 def main_page(request):
@@ -10,3 +14,27 @@ def forecasts(request):
         return render(request, 'stock/forecasts.html')
     else:
         return redirect('accounts:login')
+
+
+def forecast_detail(request, stock_pk):
+    stock = get_object_or_404(Stock, pk=stock_pk)
+    context = {
+        'stock': stock,
+    }
+    return render(request, 'stock/forecast_detail.html', context)
+
+
+def interest(request, stock_pk):
+    user = request.user
+    stock = get_object_or_404(Stock, pk=stock_pk)
+    if stock.interest_users.filter(pk=user.pk).exists():
+        stock.interest_users.remove(user)
+        interested = False
+    else:
+        stock.interest_users.add(user)
+        interested = True
+    context = {
+        'count': stock.like_users.count(),
+        'interested': interested,
+    }
+    return JsonResponse(context)
