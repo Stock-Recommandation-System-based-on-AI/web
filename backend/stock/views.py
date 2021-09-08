cc9d34ced9bd589500a849f2b9c3659fb81af44b
ketkat001 <ketkat001@gmail.com>
2021-09-08 18:44:42 +0900

    backend|stock views.py|crawling naver stock data for using analysis stock

--- backend/stock/views.py
+++ backend/stock/views.py
@@ -1,8 +1,15 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.http import HttpResponse, JsonResponse
+from django.core.paginator import Paginator
+from django.utils.dateformat import DateFormat
 from .models import Stock
+from bs4 import BeautifulSoup
+from pykrx import stock
+from datetime import date, timedelta
+import requests, json
+import pandas as pd
 
-
+today = (date.today() - timedelta(1)).strftime('%Y%m%d')
 
 # Create your views here.
 def main_page(request):
@@ -11,15 +18,73 @@ def main_page(request):
 
 def forecasts(request):
     if request.user.is_authenticated:
-        return render(request, 'stock/forecasts.html')
+        stocks = Stock.objects.all()
+        paginator = Paginator(stocks, 6)
+        page_number = request.GET.get('page')
+        page_obj = paginator.get_page(page_number)
+        context = {
+            'page_obj': page_obj,
+        }
+        return render(request, 'stock/forecasts.html', context)
     else:
         return redirect('accounts:login')
 
 
 def forecast_detail(request, stock_pk):
-    stock = get_object_or_404(Stock, pk=stock_pk)
+    stock_detail = get_object_or_404(Stock, pk=stock_pk)
+
+    urls = 'https://finance.naver.com/item/news.nhn?code=' + stock_detail.id
+    headers = {'User-Agent': "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.212 Safari/537.36"}
+    res = requests.get(urls,headers=headers)
+    soups = BeautifulSoup(res.text,'html.parser')
+    iframe_urls = soups.find_all('iframe')[3:5]
+    news_iframe_url = iframe_urls[0]['src']
+    disclosure_iframe_url = iframe_urls[1]['src']
+
+    urls = 'https://finance.naver.com' + news_iframe_url
+    res = requests.get(urls, headers=headers)
+    soups = BeautifulSoup(res.text, 'html.parser')
+    tr_list = soups.select('html > body > div.tb_cont > table.type5 > tbody > tr')[:5]
+    news = []
+    for tr in tr_list:
+        title = tr.find('td', {'class': 'title'}).text
+        link = 'https://finance.naver.com' + tr.find('a', {'class': 'tit'}).get('href')
+        report = tr.find('td', {'class': 'info'}).text
+        date = tr.find('td', {'class':'date'}).text
+        obj = {
+            'title': title,
+            'link': link,
+            'report': report,
+            'date': date,
+        }
+        news.append(obj)
+
+    urls = 'https://finance.naver.com' + disclosure_iframe_url
+    res = requests.get(urls, headers=headers)
+    soups = BeautifulSoup(res.text, 'html.parser')
+    tr_list = soups.select('html > body > div.tb_cont > table.type6 > tbody.first > tr')
+    disclosure = []
+    for tr in tr_list:
+        title = tr.find('td', {'class': 'title'}).text
+        link = 'https://finance.naver.com' + tr.find('a', {'class': 'tit'}).get('href')
+        report = tr.find('td', {'class': 'info'}).text
+        date = tr.find('td', {'class':'date'}).text
+        obj = {
+            'title': title,
+            'link': link,
+            'report': report,
+            'date': date,
+        }
+        disclosure.append(obj)
+
+    df = stock.get_market_fundamental_by_date(today, today, stock_pk)
+    stock_data = df.to_dict('records')
+    
     context = {
-        'stock': stock,
+        'stock_detail': stock_detail,
+        'news': news,
+        'disclosure': disclosure,
+        'stock_data': stock_data,
     }
     return render(request, 'stock/forecast_detail.html', context)
 
