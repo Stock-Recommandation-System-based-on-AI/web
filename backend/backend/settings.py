71efa67a4cc8b4c7685389edf013a2409eff32ec
ketkat001 <ketkat001@gmail.com>
2021-09-13 17:45:18 +0900

    backend|update database

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 import os
 import sys
-
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -21,8 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '6jtqmmfau#=z0=4bwmk6xnl_#pjnf$ptff5(kl#ax^f(&7@pb4'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -86,11 +84,18 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'stock_deploy',
+        'USER': 'root',
+        'PASSWORD': '12356Rkd!@',
+        'HOST': "localhost",
+        'PORT': "3306"
     }
 }
 
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = '6jtqmmfau#=z0=4bwmk6xnl_#pjnf$ptff5(kl#ax^f(&7@pb4'
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
