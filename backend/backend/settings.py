56c6f3b0335df461f265c53485da1c9d0ec079a1
ketkat001 <ketkat001@gmail.com>
2021-09-13 15:22:14 +0900

    backend|aws setting|update staticfiles route

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -130,8 +130,5 @@ USE_TZ = False
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static")
-]
 
 AUTH_USER_MODEL = 'accounts.User'
