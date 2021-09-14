e5ce85eeb333d188dccce6b092e022b095f613ae
ketkat001 <ketkat001@gmail.com>
2021-09-15 03:50:30 +0900

    backend|community model|update community model for change community logic

--- backend/community/models.py
+++ backend/community/models.py
@@ -1,10 +1,14 @@
 from django.db import models
 from django.conf import settings
+from stock.models import Stock
 
 # Create your models here.
 class Post(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="user_post")
+    stock = models.CharField(max_length=100)
     title = models.CharField(max_length=100)
     content = models.TextField()
+    predict_date = models.DateField()
+    position = models.CharField(max_length=100)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
