58e00060b5ce82779684469a256bda64939db03d
ketkat001 <ketkat001@gmail.com>
2021-09-15 03:49:58 +0900

    backend|community form|modify PostForm for update community logic

--- backend/community/forms.py
+++ backend/community/forms.py
@@ -3,6 +3,39 @@ from .models import Post
 
 
 class PostForm(forms.ModelForm):
+    title = forms.CharField(
+        widget=forms.TextInput(
+            attrs={
+                'class': 'form-control',
+                'placeholder': "글 제목을 입력해주세요",
+            }
+        )
+    )
+    content = forms.CharField(
+        widget=forms.Textarea(
+            attrs={
+                'class': 'form-control',
+                'placeholder':"이렇게 예측하신 이유를 작성해주세요",
+            }
+        )
+    )
+    position = forms.CharField(
+        widget=forms.TextInput(
+            attrs={
+                'class': 'form-control',
+                'list': 'position-list',
+                'autocomplete': 'off',
+            }
+        )
+    )
+    stock = forms.CharField(
+        widget=forms.TextInput(
+            attrs={
+                'class': 'form-control',
+                'list': 'stock-list',
+            }
+        )
+    )
     class Meta:
         model = Post
-        fields = ['title', 'content']
+        fields = ("title", "stock", "content", "position", "predict_date")
