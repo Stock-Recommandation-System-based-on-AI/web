41327081b8325a3ea080abada93342b5905ff15f
ketkat001 <ketkat001@gmail.com>
2021-09-15 16:09:39 +0900

    backend|community models.py|update new community model

--- backend/community/models.py
+++ backend/community/models.py
@@ -10,5 +10,6 @@ class Post(models.Model):
     content = models.TextField()
     predict_date = models.DateField()
     position = models.CharField(max_length=100)
+    price = models.IntegerField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
