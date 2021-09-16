471c7576f7ec65837a6c9bbc9e4972b042c02e1f
ketkat001 <ketkat001@gmail.com>
2021-09-16 16:15:22 +0900

    backend|stock model|update stock model for adding stock price

--- backend/stock/models.py
+++ backend/stock/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.conf import settings
+from django.db.models.deletion import CASCADE
 # Create your models here.
 
 class Stock(models.Model):
@@ -10,3 +11,9 @@ class Stock(models.Model):
     position = models.CharField(max_length=100, blank=True)
     probability = models.FloatField()
     interest_users = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name='interest_stock', blank=True)
+
+
+class Stock_price(models.Model):
+    stock = models.ForeignKey(Stock, related_name="stock_price", on_delete=models.CASCADE)
+    date = models.DateField()
+    price = models.IntegerField()
