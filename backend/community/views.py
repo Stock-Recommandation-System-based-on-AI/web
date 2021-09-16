73ffdb6508a3f2973e343e0208a4730670390ca3
ketkat001 <ketkat001@gmail.com>
2021-09-16 16:18:48 +0900

    backend|community views.py|update community views.py for adding price

--- backend/community/views.py
+++ backend/community/views.py
@@ -2,11 +2,29 @@ from django.shortcuts import render, redirect, get_object_or_404
 from django.core.paginator import Paginator
 from .models import Post
 from .forms import PostForm
-from stock.models import Stock
+from stock.models import Stock, Stock_price
+from datetime import date, timedelta, datetime
 
 # Create your views here.
+# 17시가 지나면 당일의 데이터를 불러온다. 17시 전이라면 전날의 데이터를 불러온다.
+time_data = datetime.now().strftime("%H:%M:%S")
+if int(time_data[:2]) > 16:
+    d = 0
+else:
+    d = 1
+today = (date.today() - timedelta(d)).strftime('%Y-%m-%d')
+# pykrx 라이브러리에서는 주말날짜의 데이터를 지원하지 않는다. 주말인경우 금요일로 날짜를 맞춘다.
+if date(int(today[:4]), int(today[5:7]), int(today[8:])).weekday() > 4:
+    minus_day = date(int(today[:4]), int(today[4:6]), int(today[6:])).weekday() - 4
+    today = (date.today() - timedelta(minus_day) - timedelta(d)).strftime('%Y-%m-%d')
+
+
 def post_list(request):
     posts = Post.objects.order_by('-pk')
+    for post in posts:
+        stock = Stock.objects.get(name=post.stock)
+        today_price = Stock_price.objects.get(stock=stock.pk, date=today).price
+        post.today_price = today_price
     paginator = Paginator(posts, 10)
     page_number = request.GET.get('page')
     page_obj = paginator.get_page(page_number)
@@ -23,7 +41,10 @@ def post_create(request):
             form = PostForm(request.POST)
             if form.is_valid():
                 form = form.save(commit=False)
+                stock = Stock.objects.get(name=form.stock)
+                stock_price = Stock_price.objects.get(stock=stock.pk, date=today)
                 form.user = request.user
+                form.current_price = stock_price.price
                 form.save()
                 return redirect('community:post_list')
         else:
@@ -43,6 +64,8 @@ def post_detail(request, post_pk):
     if request.user.is_authenticated:
         post = get_object_or_404(Post, pk=post_pk)
         stock = Stock.objects.all().get(name=post.stock)
+        today_price = Stock_price.objects.get(stock=stock.pk, date=today).price
+        post.today_price = today_price
         context = {
             'post': post,
             'stock': stock,
